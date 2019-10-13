0e3c08d0817c0c8f9724caf21dff96a73fdc0430
Jiahao Zhu <jiahaozhu0310@gmail.com>
2019-10-12 20:42:58 -0400

    first commit

---
+++ ARMA.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 12 17:48:25 2019
+
+@author: Ming,Jiahao
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import fmin
+import math
+
+
+class AR(object):
+    '''
+    AR(p) model, first trained, and then used to make prediction
+    '''
+    def __init__(self,p):
+        self.p=p 
+        self.train_data=None #training data
+        self.data=None #saved data used to make prediction
+        self.coeff=None
+        
+    def train(self,train_data):
+        '''
+        Train the data using train_data
+        '''
+        self.train_data=train_data
+        self.data=train_data[-self.p:]
+        initials=[0.1]*(self.p + 1) # p phi and 1 sigma
+        
+        self.coeff=fmin(self.MLE,initials)
+
+    def MLE(self,initial):
+        '''
+        MLE used to solve the coefficients of AR(p) model
+        '''
+    
+        Phi,Sigma=np.array(initial[:-1]),initial[-1]
+        N = len(self.train_data)
+        Z=[0]*N
+        Z[0]=0
+        Summation = 0
+        for i in range(self.p,N):
+            'X is from data initialization'
+            Z[i]=self.train_data[i]-Phi.dot(np.array(self.train_data[i-self.p:i]).T)
+            #Z[i]=X[i]-Phi*Z[i-1] MA(1) model
+            Summation += -1*((Z[i]**2)/(2*Sigma**2))
+        res=(-1*(N-1)/2) * np.log(2*math.pi)-((N-1)/2) * np.log(Sigma **2) + Summation
+        return -res
+    
+    def predict(self,test_data):
+        '''
+        pass one point and make one prediction
+        '''
+        if len(test_data)>1: print('pass one point only')
+        res = np.array(self.coeff[:-1]).dot(np.array(self.data[-self.p:]).T)
+        self.data.extend(test_data)
+        self.data.pop(0)
+        return res
+    
+class MA(object):
+    '''
+    MA(q) model, first trained, and then used to make prediction
+    '''
+    def __init__(self,q):
+        self.q=q 
+        self.train_data=None #training data
+        self.data=None #saved data used to make prediction
+        self.coeff=None
+        
+    def train(self,train_data):
+        '''
+        Train the data using train_data
+        '''
+        self.train_data=train_data
+        self.data=train_data[-self.q:]
+        initials=[0.1]*(self.q + 1) # p phi and 1 sigma
+        
+        self.coeff=fmin(self.MLE,initials)
+
+    def MLE(self,initial):
+        '''
+        MLE used to solve the coefficients of MA(q) model
+        '''
+    
+        Theta,Sigma=np.array(initial[:-1]),initial[-1]
+        N = len(self.train_data)
+        Z=[0]*N
+        Z[0]=0
+        Summation = 0
+        for i in range(self.q,N):
+            'X is from data initialization'
+            Z[i]=self.train_data[i]-Theta*Z[i-1] #MA(1) model
+            Summation += -1*((Z[i]**2)/(2*Sigma**2))
+        res=(-1*(N-1)/2) * np.log(2*math.pi)-((N-1)/2) * np.log(Sigma **2) + Summation
+        return -res
+    
+    def predict(self,test_data):
+        '''
+        pass one point and make one prediction
+        '''
+        if len(test_data)>1: print('pass one point only')
+        self.data.append(test_data)
+        self.data.pop(0)
+        return np.array(self.coeff[:-1]).dot(np.array(test_data[-self.q:]).T)
---
+++ plot_weight.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 12 19:04:25 2019
+
+@author: Jiahao
+
+This file generates weight change plots over time of expert
+"""
+
+src_path = '/Users/Jiahao/Documents/classes/capstone/online_learning/'
+import os
+os.chdir(src_path)
+
+from online_learner import *
+from ARMA import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+'''
+On uniform known data set
+'''
+# generate ar2 data
+phi1,phi2=0.6,0.3 # xt = 0.6x_(t-1) + 0.3 x_(t-2) + Z
+sigma=10
+N=20000
+z=np.random.normal(0,sigma,N)
+data_ar2=[0]*N
+for i in range(2,N):
+    data_ar2[i]=phi1*data_ar2[i-1] + phi2*data_ar2[i-2] + z[i]
+
+# split ar2 generated data
+data_ar2_train = data_ar2[:10000]
+data_ar2_test = data_ar2[10000:]
+
+# build ar 1,2,3,4 four models, and train those models using the first 10000 data
+n = 4 # number of experts
+ar1 = AR(1)
+ar2 = AR(2)
+ar3 = AR(3)
+ar4 = AR(4)
+ars = [ar1,ar2,ar3,ar4]
+for ar in ars:
+    ar.train(data_ar2_train)
+
+# warm up prediction
+for i in range(50):
+    for ar in ars:
+        ar.predict([data_ar2_test[i]])
+# put four experts into online learning algorithms
+exp1 = exponential_weighted_average(ars,0.05)
+exp2 = exponential_weighted_average(ars,0.6)
+exp3 = exponential_weighted_average(ars,0.1,redis=0.5)
+
+weights = [[]for i in range(n)]
+
+for point in data_ar2_test[50:]:
+    exp1.update_point([point],[point])
+    W = exp1.get_weight()
+    for i in range(n):
+        weights[i].append(W[i])
+
+for weight in weights:
+    plt.plot(weight[:10])
+plt.legend(['ar1','ar2','ar3','ar4'])
+plt.show()
+
+
+    
+
+
+
+    
+    
+
+
+
+
