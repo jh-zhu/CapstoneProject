b728e892b7495a12f002d1b6b5fb48819e315081
Jiahao Zhu <jiahaozhu0310@gmail.com>
2019-11-11 18:20:31 -0500

    update select expert

--- core/select_expert.py
+++ core/select_expert.py
@@ -3,31 +3,35 @@
 """
 Created on Mon Oct 28 01:42:35 2019
 
-@author: yitongcai
+@author: yitongcai,Jiahao
 """
 
 from experts import *
-from utils.data_generator import *
+from data_generator import *
 import numpy as np
+import pandas as pd
+import sys
+import os
 
 '''
-python expert.py(0) SVR(1) 1,2,3(2)
-import sys
-model, hyper = sys.argv[1], sys.argv[2]
-test_expert (import expert.py, take data), arg {model, hyper}  ->  loss 
+This file is to be used on hpc
+
+We use it to test single expert.
+This file reports single expert's prediction and loss on testing data
 '''
 class test_expert(object):
     
-    def  __init__(self, expert, hyperparams):
-        self.expert = expert
+    def __init__(self, expert,hyperparams,train_data,test_data):
+        
         def is_number(s):
             try:
                 float(s)
                 return True
             except ValueError:
                 return False
-        
+            
         self.hyperparams = [float(p) if is_number(p) else p for p in hyperparams.split(",") ]  #[hyper1,hyper2,hyper3,...]
+        
         if expert == "AR":
             self.model = AR(self.hyperparams[0])
         elif expert == "LR":
@@ -35,48 +39,77 @@ class test_expert(object):
         elif expert == "SVR":
             self.model = SVR(*self.hyperparams) 
         else:
-            raise Exception('{} has not been provided yet'.format(self.expert))
+            raise Exception('{} has not been implemented yet'.format(self.expert))
+        
+        if train_data is not None:
+            df_train = pd.read_csv(train_data)
+            self.y_train = np.array(df_train.iloc[:,0])
+            self.X_train = np.array(df_train.iloc[:,1:])
             
+            del df_train
+        else: 
+            X_train,y_train = self.generate_data()
+        
+        if test_data is not None:
+            df_test = pd.read_csv(test_data)
+            self.y_test = np.array(df_test.iloc[:,0])
+            self.X_test = np.array(df_test.iloc[:,1:])
+            
+            del df_test
+        else:
+            X_test,y_test = self.generate_data()
+        
+    def generate_data():
+        '''
+        If no data path is provided, this module generate its own data
+        '''
+        N = 1000
+        sigma = 1
+        kernel = 'linear_regression' 
+        parameter = [1,[1,2,1.5]]
+        generator = data_generator(kernel,parameter,sigma,N)
+        return generator.generate()
+    
     def train_test(self):
-        '''Get the real dataset with X, y and split into train set and test set'''
+        # train
+        self.model.train(self.X_train,self.y_train)
         
-        
-        '''Generate data from specific model for training'''
-        sigmas=np.arange(0,10,1)
-        N = 1000 # number of data to generate
-        noise_level = 1 # level of noise (sigma), added to the generating process
-        kernel = 'linear_regression' # the model to generate 
-        parameter = [1,[1,2,1.5],noise_level,N] # parameter of data generating model
-        
-        generator = data_generator(kernel,parameter,noise_level,N)
-        X_train,y_train = generator.generate()
-        X_test,y_test = generator.generate()
-        self.model.train(X_train,y_train)
-        
+        # test
         preds, losses=[], []
-        for x,y in zip(X_test,y_test):
+        for x,y in zip(self.X_test,self.y_test):
             y_pred = self.model.predict(x)
-            MSE = abs(y - y_pred)**2
-            losses.append(MSE)
+            loss = (y - y_pred)**2
+            losses.append(loss)
             preds.append(y_pred)
         return (losses, preds)
-            
-import sys
-expert, hyper = sys.argv[1], sys.argv[2]   
-test_expert = test_expert(expert, hyper)
-losses, preds = test_expert.train_test()
-
-import csv 
-header = [expert+"_"+hyper+'_Prediction', expert+"_"+hyper+'_Losses']
-with open("/Users/yitongcai/Downloads/"+expert+"_"+hyper+".csv", 'w', newline='') as f:
-    writer = csv.writer(f, delimiter=',')
-    writer.writerow(header) 
-    for p,l in zip(preds, losses):
-        writer.writerow([p,l])
-
-
-        
     
+if __name__ == '__main__':
+    '''
+    Input argvs:  expert name, hyperparameter, train data path,
+                  test data path, output directory path
+    
+    '''
+    # test expert
+    model_parameters = sys.argv[1:-1]
+    test_expert = test_expert(*model_parameters)
+    losses, preds = test_expert.train_test()
+    
+    # write out results
+    output_directory = sys.argv[-1]
+    file_name = model_parameters[0] + '_' + model_parameters[1] + '.csv'
+    output_file_path = os.path.join(output_directory,file_name)
+    
+    columns = ['prediction','loss']
+    df_out = pd.DataFrame(np.array([preds,losses]).T,columns = columns)
+    df_out.to_csv(output_file_path,index=False)
+    
+    
+
+
+
+
+
+   
 #    def RandomCreate(self, **kwargs):
 #        ''' Use the random grid to search for best hyperparameters'''
 #        random_range = {k:v for k, v in kwargs.items()}
--- hpc/createSfile.py
+++ hpc/createSfile.py
@@ -5,6 +5,8 @@ Created on Sat Nov  9 10:38:23 2019
 
 @author: mingmingyu
 """
+import numpy as np
+
 n_nodes=10 #number of node
 n_tasks=10 #number of tasks
 tpn=int(n_tasks/n_nodes) #task per node
@@ -27,9 +29,17 @@ cd /scratch/mmy272/CapstoneProject/core \n\
 
 ## run python tasks in cluster in parallel
 #change the middle part
-file.write(f'\
-srun -N 1 -n 1 python3 select_expert.py '+'SVR rbf,0.1,1' +' & \n\
-')
+
+train_data_path = '/Users/Jiahao/Downloads/train.csv'
+test_data_path = '/Users/Jiahao/Downloads/test.csv'
+output_directory = '/Users/Jiahao/Downloads/output/'
+
+gammas = np.arange(0.01,0.05,0.01)
+C = np.arange(0.1,0.5,0.1)
+
+for gamma,c in zip(gammas,C):
+    file.write(f'\
+    srun -N 1 -n 1 python3 select_expert.py '+'SVR linear,{},{} {} {} {}'.format(gamma,c,train_data_path,test_data_path,output_directory) +' & \n')
 
 file.write('wait ')
 
