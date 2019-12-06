1ccc0873741c52f84e70e7c2b4036a9215796a1c
Ming ming Yu <mingyu9876@gmail.com>
2019-12-05 23:21:22 -0500

    update

--- hpc/createSfile3.py
+++ hpc/createSfile3.py
@@ -9,6 +9,7 @@ from generateParameter import *
 import numpy as np
 from hpc.generateParameter import generateParameter as GP
 from core.fileManager import fileName
+import random,itertools
 import os
 
 n_nodes=5 #number of node
@@ -32,7 +33,6 @@ cd /scratch/mmy272/test/scripts \n\
 
 kernels = ['rbf', 'linear']
 nums={800:[6,6,8], 600:[5,6,6], 400:[5,5,5], 200:[3,3,4], 100:[2,2,4]}
-gammas, Cs, epsilons = gen_params(nums, "SVR")
 
 for exp_num,value in nums.items():
     gammas, Cs, epsilons = gen_params(value, "SVR")
@@ -40,7 +40,7 @@ for exp_num,value in nums.items():
         for gamma in gammas:
             for C in Cs:
                 for epsilon in epsilons:
-                    file.write('srun -N 1 -n 1 python3 select_expert.py SVR {},{},{},{} G '.format(kernel, gamma,C, epsilon) +str(exp_num)+' $SLURM_ARRAY_TASK_ID.txt & \n')
+                    file.write('srun -N 1 -n 1 python3 select_expert_2.py SVR {},{},{},{} G '.format(kernel, gamma,C, epsilon) +str(exp_num)+' $SLURM_ARRAY_TASK_ID.txt & \n')
 
 
 
@@ -50,53 +50,53 @@ for exp_num,value in nums.items():
     random_params = random.sample(list(itertools.product(kernels, gammas, Cs, epsilons)), num_experts)
     for params in random_params:
         kernel, gamma,C, epsilon = params
-        file.write('srun -N 1 -n 1 python3 select_expert.py SVR {},{},{},{} R '.format(kernel, gamma,C, epsilon)+str(exp_num) +' $SLURM_ARRAY_TASK_ID.txt & \n')
+        file.write('srun -N 1 -n 1 python3 select_expert_2.py SVR {},{},{},{} R '.format(kernel, gamma,C, epsilon)+str(exp_num) +' $SLURM_ARRAY_TASK_ID.txt & \n')
 
 
 
 nums={800:[5,7], 600:[5,7], 400:[4,6], 200:[4,6], 100:[3,5]}
 for exp_num,value in nums.items():
-    alphas, l1s = gen_params(nums, "LR")
+    alphas, l1s = gen_params(value, "LR")
     for alpha in alphas:
-        for l1 in l1s:        
-            file.write('srun -N 1 -n 1 python3 select_expert.py LR {},{} G '.format(alpha,l1) +str(exp_num)+' $SLURM_ARRAY_TASK_ID.txt & \n')
+        for l1 in l1s:
+            file.write('srun -N 1 -n 1 python3 select_expert_2.py LR {},{} G '.format(alpha,l1) +str(exp_num)+' $SLURM_ARRAY_TASK_ID.txt & \n')
 
 
 for exp_num,value in nums.items():
-    alphas,l1s = gen_params_random(nums, "LR")
+    alphas,l1s = gen_params_random(value, "LR")
     num_experts = len(alphas)*len(l1s)
     random_params = random.sample(list(itertools.product(alphas,l1s)), num_experts)
     for params in random_params:
         alpha,l1 = params
         for alpha in alphas:
-            for l1 in l1s:        
-                file.write('srun -N 1 -n 1 python3 select_expert.py LR {},{} R '.format(alpha,l1) +str(exp_num)+' $SLURM_ARRAY_TASK_ID.txt & \n')
+            for l1 in l1s:
+                file.write('srun -N 1 -n 1 python3 select_expert_2.py LR {},{} R '.format(alpha,l1) +str(exp_num)+' $SLURM_ARRAY_TASK_ID.txt & \n')
 
 
 
 nums={800:[4,5,2,2], 600:[4,5,2,2], 400:[4,5,2,2], 200:[3,4,2,2], 100:[2,3,2,2]}
 
 for exp_num,value in nums.items():
-    n_estimators, max_depth, min_samples_split, min_samples_leaf = gen_params(nums, "RF")
+    n_estimators, max_depth, min_samples_split, min_samples_leaf = gen_params(value, "RF")
     max_features = ['auto', 'sqrt']
     for n in n_estimators:
         for depth in max_depth:
             for split in min_samples_split:
                 for leaf in min_samples_leaf:
                     for feature in max_features:
-                        file.write('srun -N 1 -n 1 python3 select_expert.py RF {},{},{},{},{} G '.format(n, depth, split, leaf, feature)+str(exp_num)+' $SLURM_ARRAY_TASK_ID.txt & \n')           
+                        file.write('srun -N 1 -n 1 python3 select_expert_2.py RF {},{},{},{},{} G '.format(n, depth, split, leaf, feature)+str(exp_num)+' $SLURM_ARRAY_TASK_ID.txt & \n')
 
 
 for exp_num,value in nums.items():
-    n_estimators, max_depth, min_samples_split, min_samples_leaf = gen_params_random(nums, "RF")
+    n_estimators, max_depth, min_samples_split, min_samples_leaf = gen_params_random(value, "RF")
     max_features = ['auto', 'sqrt']
     num_experts = len(max_features)*len(n_estimators)*len(max_depth)*len(min_samples_split)*len(min_samples_leaf)
     random_params = random.sample(list(itertools.product(n_estimators, max_depth, min_samples_split, min_samples_leaf, max_features)), num_experts)
     for params in random_params:
-        n, depth, split, leaf, feature = params                           
-        file.write('srun -N 1 -n 1 python3 select_expert.py RF '+ '{},{},{},{},{} R '.format(n, depth, split, leaf, feature)+str(exp_num)+' $SLURM_ARRAY_TASK_ID.txt & \n')      
+        n, depth, split, leaf, feature = params
+        file.write('srun -N 1 -n 1 python3 select_expert_2.py RF '+ '{},{},{},{},{} R '.format(n, depth, split, leaf, feature)+str(exp_num)+' $SLURM_ARRAY_TASK_ID.txt & \n')
 
-        
+
 # =============================================================================
 # nums = [2, 3, 2, 2, 2, 2, 1, 1]
 # n_estimators, max_depth, learning_rate, subsample, colsample_bytree, gammas, alphas, lambd = gen_params(nums, "XGBoost")
@@ -117,3 +117,4 @@ for exp_num,value in nums.items():
 file.write('wait ')
 
 file.close()
+                                             
